ecfb484770adb4c6265cd83d7bde38d677c84b04
Justin Estrada Cruz <justinestradacruz2006@gmail.com>
2026-02-18 23:23:53 -0600

    Prettier movie details, not finished yet

--- src/features/movies/MovieDetails.tsx
+++ src/features/movies/MovieDetails.tsx
@@ -1,12 +1,18 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { getMovieByID } from "../../service/Movies.service";
 import type { MovieDB } from "../../models/MovieDB.model";
+import { CiWarning } from "react-icons/ci";
+import GridSpinner from "../loader/GridSpinner";
+import { IoIosHome } from "react-icons/io";
+import { MdFavoriteBorder,MdFavorite } from "react-icons/md";
+import { toast } from "react-toastify";
 
 export default function MovieDetails() {
   const { id } = useParams<{ id: string }>();
   const [movie, setMovie] = useState<MovieDB | null>();
   const [loading, setLoading] = useState<boolean>(true);
+  const [favorite, setFavorite] = useState<boolean>(false);
 
   useEffect(() => {
     const loadMovie = async () => {
@@ -23,48 +29,67 @@ export default function MovieDetails() {
     loadMovie();
   }, []);
 
+  const toggleFavorite = () => {
+    setFavorite(!favorite);
+    if(favorite){
+      toast.success('Movie saved successfully',{
+        autoClose:1300
+      });
+    }else{
+      toast.clearWaitingQueue();
+      toast.success('Movie unsaved successfully',{
+        autoClose:1300
+      });
+    }
+  }
+
   if (loading) {
     return (
-      <div className="min-h-screen bg-[#070B16] text-white p-6">
-        ... Cargando
+      <div className="">
+        <GridSpinner loading={loading} />
       </div>
     )
   }
 
   if (!loading && !movie) {
     return (
-      <h3 className="text-4xl text-center">Movie not found!</h3>
+      <div className="flex flex-col justify-center items-center h-screen bg-linear-to-b from-white to-black">
+        <h3 className="text-4xl">Movie not found!</h3>
+        <CiWarning className="text-9xl" />
+        <Link to={'/'} className=" bg-black text-lg text-white px-7 py-3 rounded-2xl hover:bg-neutral-900">Return home</Link>
+      </div>
     );
   }
 
   return (
-    <div className="">
+    <div className="h-auto lg:h-screen">
       <h1 className="text-4xl pl-3">Movie Details</h1>
       <div className="flex justify-center py-10">
         <div className="flex flex-col space-y-8 lg:space-y-0 lg:flex-row bg-neutral-300 rounded-4xl w-400 shadow-2xl px-10 py-4 space-x-4 ">
           <img src={`https://image.tmdb.org/t/p/w500${movie!.poster_path}`} alt="movie poster" className="rounded-3xl " />
-          <div className="bg-neutral-400 shadow-2xl rounded-3xl flex-1 pl-3 pt-4 space-y-5">
+          <div className="flex flex-col justify-between bg-neutral-400 shadow-2xl rounded-3xl flex-1 px-3 py-4 ">
             <span className="flex items-end space-x-2">
-              <p className="text-4xl font-semibold">Movie name:</p>
-              <p className="text-5xl ">{movie!.title}</p>
+              <p className="text-4xl font-semibold">Name:</p>
+              <p className="text-4xl ">{movie!.title}</p>
             </span>
-            <span className="flex items-start space-x-2">
+            {/* sm:flex sm:items-start sm:space-x-2 */}
+            <span className="">
               <p className="text-4xl font-semibold">Description:</p>
-              <p className="text-4xl ">{movie!.overview}</p>
+              <p className="text-4xl px-3 text-neutral-700 pb-5">{movie!.overview}</p>
             </span>
-            <div className="flex justify-between pr-10 space-x-10 flex-col lg:flex-row">
+            <div className="grid grid-cols-1 space-y-5 sm:grid-cols-2 sm:space-y-0 pr-10 space-x-10 flex-col lg:flex-row">
               <span className="flex items-start space-x-2">
                 <p className="text-3xl font-semibold">Release Date:</p>
                 <p className="text-3xl ">{movie!.release_date}</p>
               </span>
 
-              <span className="flex items-start space-x-2">
+              <span className="flex items-end sm:items-start space-x-2">
                 <p className="text-3xl font-semibold">Is this movie for adults only:</p>
-                <p className="text-3xl ">{movie!.forAdults ? ('Yes'):('No')}</p>
+                <p className="text-3xl ">{movie!.forAdults ? ('Yes') : ('No')}</p>
               </span>
 
             </div>
-            <div className="flex justify-between pr-10 space-x-10 flex-col lg:flex-row">
+            <div className="grid grid-cols-1 space-y-5 sm:grid-cols-2 sm:space-y-0 pr-10 space-x-10 flex-col lg:flex-row">
               <span className="flex items-start space-x-2">
                 <p className="text-3xl font-semibold">Rating:</p>
                 <p className="text-3xl ">{movie!.vote_average}/10</p>
@@ -74,7 +99,23 @@ export default function MovieDetails() {
                 <p className="text-3xl font-semibold">Total votes:</p>
                 <p className="text-3xl ">{movie!.vote_count}</p>
               </span>
+            </div>
+            <div className="flex justify-end space-x-5 pr-5">
+              {favorite ?
+                (<button className="flex items-center space-x-2 px-5 bg-pink-400 text-2xl rounded-md cursor-pointer hover:bg-pink-300" onClick={toggleFavorite}>
+                  <p>Remove from favorites</p>
+                  <MdFavorite className="pt-1 text-red-700" />
+                </button>) :
+                (<button className="flex items-center space-x-2 px-5 bg-pink-400 text-2xl rounded-md cursor-pointer hover:bg-pink-300" onClick={toggleFavorite}>
+                  <p>Add to favorites</p>
+                  <MdFavoriteBorder className="pt-1" />
+                </button>)
+              }
 
+              <Link to={'/'} className="flex items-center space-x-2 bg-neutral-700 text-2xl rounded-md px-5 hover:bg-neutral-600">
+                <p>Return home</p>
+                <IoIosHome />
+              </Link>
             </div>
           </div>
         </div>
--- src/features/search/SearchMovie.tsx
+++ src/features/search/SearchMovie.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, type ReactNode } from "react";
+import { useEffect, useRef, useState, type ReactNode } from "react";
 import type { MovieDB } from "../../models/MovieDB.model";
 import { getMovieByName } from "../../service/Movies.service";
 import GridSpinner from "../loader/GridSpinner";
@@ -10,6 +10,7 @@ const SearchMovie = () => {
     const [movieName, setMovieName] = useState<string>('');
     const [loading, setLoading] = useState<boolean>(false);
     const [searching, setSearching] = useState<boolean>(true);
+    const ref = useRef<HTMLInputElement>(null);
 
     const bgStyle = ({ isScreenEmpty }: { isScreenEmpty: boolean }): string => {
         return 'bg-linear-to-b from-white to-black ' +
@@ -23,6 +24,8 @@ const SearchMovie = () => {
     useEffect(() => {
         if (!movie || movie.length === 0 || searching) {
             setBgStyleString(bgStyle({ isScreenEmpty: true }));
+        }else if(movie.length <= 8){
+            setBgStyleString(bgStyle({ isScreenEmpty: false }) + ' xl:h-screen');
         } else if (movie) {
             setBgStyleString(bgStyle({ isScreenEmpty: false }));
         }
@@ -38,6 +41,7 @@ const SearchMovie = () => {
             console.log(error);
         } finally {
             setLoading(false);
+            unfocusInput();
         }
     }
 
@@ -63,15 +67,28 @@ const SearchMovie = () => {
             return <MovieList movies={movie} isHome={false}/>
         }
 
-        return <p>Esto es una prueba</p>
+        return <p></p>
     }
+
+    const keyListener = (event:React.KeyboardEvent) => {
+        if(event.key === 'Enter'){
+            handleClick();
+        }
+    };
+
+    const unfocusInput = () => {
+        if(ref.current){
+            ref.current.blur();
+        }
+    };
+
 
     return (
         <div className={bgStyleString}>
             <p className="text-3xl text-center px-10 pt-3">Search a movie you want to learn about</p>
             <div className="flex justify-center py-4 ">
                 <div className="border-2 rounded-4xl text-3xl flex">
-                    <input type="text" placeholder="Movie name..." className="focus:outline-none pl-3 w-60 lg:w-223 xl:w-260" onChange={onChangeHandler} value={movieName}/>
+                    <input ref={ref} type="text" placeholder="Movie name..." className="focus:outline-none pl-3 w-60 lg:w-223 xl:w-260" onChange={onChangeHandler} value={movieName} onKeyDown={keyListener} />
                     <button className="px-4 my-1 mx-2 cursor-pointer bg-neutral-500 rounded-4xl hover:bg-neutral-600 hover:rounded-r-4xl " onClick={handleClick}>
                         <div className="">
                             <IoIosSearch />
